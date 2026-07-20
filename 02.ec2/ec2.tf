7f219939f6b5acbe1bd5d90aa7eded24d1a6c9f5
varunaita <varunaita3005@gmail.com>
2026-07-20 17:35:04 +0000

    hi

--- 02.ec2/ec2.tf
+++ 02.ec2/ec2.tf
@@ -1,11 +1,3 @@
-terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "6.17.0"
-        }
-    }
-}
 
 resource "aws_instance" "ec2"{
     ami = ami-0220d79f3f480ecf5
